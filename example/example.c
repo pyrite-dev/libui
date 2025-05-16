f7d6aa9f3a854abf4ded84ddff19b637e2c45b6a
NishiOwO <nishi@nishi.boats>
2025-05-16 13:27:42 +0900

    fix

--- example/example.c
+++ example/example.c
@@ -3,16 +3,19 @@
 int main(){
 	libui_t* ui = libui_create("test", 0, 0, 200, 200);
 	libui_widget_t* w;
+	int i;
 
-	w = libui_new_widget(ui);
-	w->text = "Test button";
-	w->type = LIBUI_BUTTON;
-	w->origin_x = LIBUI_RIGHT;
-	w->origin_y = LIBUI_BOTTOM;
-	w->x = 0;
-	w->y = 0;
-	w->width = 100;
-	w->height = 100;
+	for(i = 0; i < (200 / 50) * (200 / 25); i++){
+		w = libui_new_widget(ui);
+		w->text = "Test";
+		w->type = LIBUI_BUTTON;
+		w->origin_x = LIBUI_LEFT;
+		w->origin_y = LIBUI_TOP;
+		w->x = (i % 4) * 50;
+		w->y = (i / 4) * 25;
+		w->width = 50;
+		w->height = 25;
+	}
 
 	libui_loop(ui);
 	libui_destroy(ui);
--- include/libui_config.h
+++
@@ -1,4 +0,0 @@
-#ifndef __LIBUI_CONFIG_H__
-#define __LIBUI_CONFIG_H__
-#define LIBUI_MOTIF
-#endif
